58401c2956ba2c5a7ff6bfb426f27a5164b3e710
spalominor <spalominor@unal.edu.co>
2024-04-13 01:00:42 -0500

    pandas, scipy, structure, data

---
+++ app/formulario_pedidos.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+import data
+import time
+
+def main():
+    """
+    Función principal que ejecuta la aplicación de registro de nuevo pedido.
+    """
+    st.title('Registro de Nuevo Pedido')
+
+    # Crear un diccionario para almacenar los datos del formulario
+    form_data = {
+        "direccion_pedido": None,
+        "fecha_pedido": None,
+        "cliente": None,
+        "estado_pedido": None,
+    }
+
+    # Formulario
+    with st.form('Registro de Nuevo Pedido'):
+        form_data["direccion_pedido"] = st.text_input('Dirección del pedido:')
+        form_data["cliente"] = st.text_input('Nombre del cliente:')
+        form_data["estado_pedido"] = st.selectbox('Estado del pedido:', ['Pendiente', 'En camino', 'Entregado'])
+
+        submitted = st.form_submit_button('Registrar Pedido')
+
+    # Procesamiento de los datos
+    if submitted:
+        # Capturar la fecha y hora en la que se registró el pedido
+        form_data["fecha_pedido"] = pd.Timestamp.now().strftime('%Y-%m-%d %H:%M:%S')
+        # Crear un DataFrame con los datos del formulario
+        form_df = pd.DataFrame([form_data.values()], columns=form_data.keys())
+        # Actualizar la información en la base de datos
+        data.actualizar_informacion('pedidos', form_df)
+        # Mostrar mensaje de éxito
+        success_message = st.empty()
+        success_message.success('¡Pedido registrado exitosamente!')
+        # Esperar 3 segundos antes de limpiar el mensaje
+        time.sleep(3)
+        success_message.empty()
+
+if __name__ == '__main__':
+    main()
---
+++ app/visualizar_pedidos.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import data as data
+
+def main():
+    """
+    Visualización de pedidos.
+    """
+    st.title('Visualización de Pedidos')
+
+    # Cargar datos de los pedidos
+    df_pedidos = data.pedidos()
+    opciones_estado = ['Todos', 'Pendiente', 'En camino', 'Entregado']
+    
+    # Filtrar y ordenar los pedidos si es necesario
+    with st.sidebar:
+        st.write('## Filtrar y ordenar pedidos')
+        activar_tabla_dinamica = st.checkbox('Activar tabla dinámica')
+
+        if activar_tabla_dinamica:
+            filtro_estado = st.selectbox('Filtrar por Estado:', opciones_estado)
+            if filtro_estado != 'Todos':
+                df_pedidos = df_pedidos[df_pedidos['estado'] == filtro_estado]
+
+            busqueda_direccion = st.text_input('Buscar por Dirección:')
+            busqueda_cliente = st.text_input('Buscar por cliente:')
+            if busqueda_direccion or busqueda_cliente:
+                df_pedidos = df_pedidos[
+                    df_pedidos['direccion'].str.contains(busqueda_direccion, case=False) & \
+                    df_pedidos['cliente'].str.contains(busqueda_cliente, case=False)
+                    ]
+                
+            busqueda_fecha = st.date_input('Buscar por fecha:', value=None)
+            if busqueda_fecha is not None:
+                df_pedidos = df_pedidos[
+                    df_pedidos['fecha'].dt.date == busqueda_fecha
+                    ]
+            
+            orden_columna = st.selectbox('Ordenar por Columna:', df_pedidos.columns)
+
+            # Botones de orden ascendente y descendente
+            orden_ascendente = st.button('Orden Ascendente')
+            orden_descendente = st.button('Orden Descendente')
+
+            # Ordenar según el botón presionado
+            if orden_ascendente:
+                df_pedidos = df_pedidos.sort_values(by=orden_columna)
+            elif orden_descendente:
+                df_pedidos = df_pedidos.sort_values(by=orden_columna, ascending=False)
+                
+
+    col_1, col_2 = st.columns([4, 1])
+    # Mostrar tabla de pedidos
+    with col_1:
+        st.write(df_pedidos)
+    
+    with col_2:
+        if st.button('Crear pedidos'):
+            st.switch_page('pages/formulario_pedidos.py')
+
+if __name__ == '__main__':
+    main()
